d70b2712e24a8c5265100d658ab5ee601befa08b
Tony David <tonydavid@MacBookTony.local>
2018-08-24 11:36:00 -0500

    Initial Commit

---
+++ Scanner/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Scanner
+//
+//  Created by Tony David on 24/08/18.
+//  Copyright © 2018 Tony David. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
